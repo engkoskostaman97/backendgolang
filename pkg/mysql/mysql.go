3b12c3a17aff7e6c536f564c82d05a34e3cb72b1
penerus programmer <engkoskostaman759@gmail.com>
2022-09-25 03:47:40 +0700

    depoloy

--- pkg/mysql/mysql.go
+++ pkg/mysql/mysql.go
@@ -20,7 +20,7 @@ func DatabaseInit() {
 	var err error
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT)
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	
+
 	// dsn := "root:@tcp(127.0.0.1:3306)/dumbflix?charset=utf8mb4&parseTime=True&loc=Local"
 	// DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
